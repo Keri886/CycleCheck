131a5f55cf74ea2cb48452ba48c49285f2b0f5a6
Keri886 <xiaoqiao.zhang@csu.fullerton.edu>
2023-12-07 04:40:43 -0800

    update settings

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,29 +1,77 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Switch, Button, Alert } from 'react-native';
 import COLORS from '../constants/colors';
-import { db, auth } from '../firebase/firebase';
-//import { useAuth } from '../firebase/auth';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from '../firebase/firebase';
 
-export default function Settings({navigation}){
+export default function Settings({ navigation }) {
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(false);
+  const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(false);
 
-     const handleSignOut = async () => {
-       await auth.signOut();
-       navigation.navigate('Welcome')
-     }
-   return (
-    <View style= {{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-      <Text>Settings Screen</Text>
+  const handleSignOut = async () => {
+    await auth.signOut();
+    navigation.navigate('Welcome');
+  };
 
-  <Button  
-                    title="Sign Out"
-                    onPress={handleSignOut}
-                    style={{
-                        marginTop: 18,
-                        marginBottom: 4,
-                    }}
-                />
+  const toggleNotifications = () => setIsNotificationsEnabled(previousState => !previousState);
+  const toggleDarkMode = () => setIsDarkModeEnabled(previousState => !previousState);
 
+  return (
+    <View style={styles.container}>
+     
+
+      <View style={styles.setting}>
+        <Text style={styles.settingLabel}>Enable Notifications</Text>
+        <Switch
+          trackColor={{ false: COLORS.lightGray, true: COLORS.primary }}
+          thumbColor={isNotificationsEnabled ? COLORS.secondary : COLORS.white}
+          onValueChange={toggleNotifications}
+          value={isNotificationsEnabled}
+        />
+      </View>
+
+      <View style={styles.setting}>
+        <Text style={styles.settingLabel}>Dark Mode</Text>
+        <Switch
+          trackColor={{ false: COLORS.lightGray, true: COLORS.primary }}
+          thumbColor={isDarkModeEnabled ? COLORS.secondary : COLORS.white}
+          onValueChange={toggleDarkMode}
+          value={isDarkModeEnabled}
+        />
+      </View>
+
+      <Button
+        title="Sign Out"
+        onPress={handleSignOut}
+        color={COLORS.secondary}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.white,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  setting: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '90%',
+    marginBottom: 20,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.lightGray,
+  },
+  settingLabel: {
+    fontSize: 16,
+    color: COLORS.secondary,
+  },
+});
