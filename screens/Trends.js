c2bfb31b008fa0f5cd367900da868852413ca1a6
Keri886 <xiaoqiao.zhang@csu.fullerton.edu>
2023-12-07 03:38:10 -0800

    Demo done, not fully function

--- screens/Trends.js
+++ screens/Trends.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity,Alert , Pressable } from 'react-native';
-import { LineChart } from 'react-native-gifted-charts';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity,Alert , Pressable, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 //npm install react-native-gifted-charts react-native-svg
 import COLORS from '../constants/colors';
 import { db, auth } from '../firebase/firebase';
 import { deleteDoc, doc ,Timestamp, collection, query, where, getDocs, orderBy,onSnapshot} from "firebase/firestore"; 
 //import { useAuth } from '../firebase/auth';
+
 
 const Trends = () => {
     const [userData, setUserData] = useState([]);
@@ -22,18 +23,33 @@ const Trends = () => {
         const data = doc.data();
         console.log(data);
         const date = data.Date?.toDate(); // Convert Timestamp to JavaScript Date object
+        // Convert the temperature string to a floating-point number
+        const temperature = parseFloat(data.Temperature);
         userData.push({
         id: doc.id,
         date: date, // Keep the Date object to use in toLocaleDateString
         temperature: data.Temperature,
       });
     });
+      
         setUserData(userData)
-       console.log("userData: ", userData.join(", "));
+        console.log("userData:", userData);
       });
       return () => unsubscribe();
  //   }
     },[]);
+
+    const chartData = {
+      labels: ["12/4", "12/5", "12/6", "12/7", "12/8", "12/9", "12/10"], // Replace with your desired date format
+      datasets: [
+        {
+          data: [36.6, 36.7, 36.5, 36.8, 36.5, 36.9, 37.0],
+          color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`,
+          strokeWidth: 2
+        }
+      ],
+      legend: ["Temperature over the week"]
+    };
 
     const handleDelete = async (id) => {
       // Confirm deletion with the user before proceeding
@@ -73,8 +89,35 @@ const Trends = () => {
 
 return (
   <View style={styles.container}>
-    <Text style={styles.header}>Measurements</Text>
-    {/* Chart here */}
+        <Text style={styles.header}>Report</Text>
+        <LineChart
+        data={chartData}
+        width={Dimensions.get('window').width - 20} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: COLORS.primary,
+          backgroundGradientFrom: COLORS.primary,
+          backgroundGradientTo: COLORS.secondary,
+          decimalPlaces: 1,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+ 
+    
     <FlatList
         data={userData}
         renderItem={renderItem}
