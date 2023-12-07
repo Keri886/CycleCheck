2c9ce08e75027b48e497ce532e65fd11e24d9037
Keri886 <xiaoqiao.zhang@csu.fullerton.edu>
2023-12-07 05:44:40 -0800

    Create Calendar Page

--- screens/Calendars.js
+++ screens/Calendars.js
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 const Calendars = () => {
-  return (
-    <View>
-      <Text>Calendars</Text>
-    </View>
-  )
-}
+  const [markedDates, setMarkedDates] = useState({});
 
-export default Calendars
+  const onDayPress = (day) => {
+    const { dateString } = day;
+    setMarkedDates((currentMarks) => {
+      if (currentMarks[dateString]) {
+        // Already marked, so unmark it
+        const updatedMarks = { ...currentMarks };
+        delete updatedMarks[dateString]; // Remove the mark for this day
+        return updatedMarks;
+      } else {
+        // Not marked, so add a mark
+        return {
+          ...currentMarks,
+          [day.dateString]: {
+            selected: true,
+            selectedColor: 'pink',
+            // Use custom styles for larger circle or different styles
+            customStyles: {
+              container: {
+                backgroundColor: 'pink',
+                borderRadius: 20,
+              },
+              text: {
+                color: 'white',
+                fontWeight: 'bold',
+              },
+            },
+          },
+        };
+      }
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Calendar
+        current={Date()}
+        onDayPress={onDayPress}
+        monthFormat={'MM/yyyy'}
+        hideExtraDays={true}
+        firstDay={1}
+        enableSwipeMonths={true}
+        markedDates={markedDates}
+        markingType={'simple'}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    paddingTop: 10,
+  },
+});
+
+export default Calendars;
